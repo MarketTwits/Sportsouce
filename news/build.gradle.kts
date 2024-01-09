cb244fd71e382cd103c2d2e02825e78bdb737bd1
daniil <daniil-kulakov@list.ru>
2024-01-10 00:04:04 +0700

    add unresolve file

---
+++ news/build.gradle.kts
@@ -0,0 +1,19 @@
+import com.android.tools.r8.internal.js
+
+plugins {
+    alias(libs.plugins.android.library.compose.convention)
+    alias(libs.plugins.kotlin.serialization)
+}
+android {
+    namespace = "com.markettwits.news"
+}
+dependencies {
+    api(project(":cloud"))
+    implementation(project(":auth"))
+    implementation(project(":core-ui"))
+    implementation ("de.charlex.compose.material3:material3-html-text:2.0.0-beta01")
+    implementation(libs.koin.core)
+    implementation(libs.bundles.decompose.compose)
+    implementation(libs.kotlinx.datetime)
+    implementation(libs.bundles.mviKotlin)
+}

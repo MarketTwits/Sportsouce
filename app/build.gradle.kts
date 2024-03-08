50326337a486fcfbc3378c515415c5e5c9a49684
daniil <daniil-kulakov@list.ru>
2024-03-08 22:02:08 +0700

    update mipmap

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
-    buildTypes {
+//    buildTypes {
 //        debug {
 //            isMinifyEnabled = true
 //            isShrinkResources = true
@@ -26,7 +26,7 @@ android {
 //                "proguard-rules.pro"
 //            )
 //        }
-    }
+//    }
 }
 
 dependencies {

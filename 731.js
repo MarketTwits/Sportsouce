2c9df4b0a7e8e10e50fbcb1017890257a781f090
MarketTwits <MarketTwits@users.noreply.github.com>
2025-03-30 12:45:02 +0000

    Deploying to gh-pages from @ MarketTwits/Sportsouce@b235ab68fa4b5284d696cb5bea17d0302d466ead 🚀

--- 731.js
+++ 731.js
@@ -1,2 +1,2 @@
-"use strict";(globalThis.webpackChunkbrowser=globalThis.webpackChunkbrowser||[]).push([[731],{731:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+"use strict";(globalThis.webpackChunkapp_browser=globalThis.webpackChunkapp_browser||[]).push([[731],{731:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
 //# sourceMappingURL=731.js.map
